e0da37912ddc982d5faabbc75c72dd6d803d38d6
Junrey Lanas <Junrey Lanas>
2026-03-10 13:02:16 +0800

    added setter injection for payment service instead of consturctor injection

--- src/main/java/com/drakkar/store/OrderService.java
+++ src/main/java/com/drakkar/store/OrderService.java
@@ -3,11 +3,15 @@ package com.drakkar.store;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(100);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
