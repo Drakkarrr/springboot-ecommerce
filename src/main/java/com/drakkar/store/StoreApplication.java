fcd5b57772faf9f57f454b75be16e538762f1f6a
Junrey Lanas <Junrey Lanas>
2026-03-10 13:26:25 +0800

    implemented application context and fixed the error on Store Application service constructor

--- src/main/java/com/drakkar/store/StoreApplication.java
+++ src/main/java/com/drakkar/store/StoreApplication.java
@@ -2,12 +2,14 @@ package com.drakkar.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        var orderService = new OrderService();
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
         orderService.setPaymentService(new StripePaymentService());
         orderService.placeOrder();
     }
