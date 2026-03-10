d9169e6f44f4c10870f66e5b06a8cf87772a0c97
Junrey Lanas <Junrey Lanas>
2026-03-10 12:53:43 +0800

    feat: added additional payment service for paypal

--- src/main/java/com/drakkar/store/PaypalPaymentService.java
+++ src/main/java/com/drakkar/store/PaypalPaymentService.java
@@ -1,9 +1,9 @@
 package com.drakkar.store;
 
-public class StripePaymentService implements PaymentService {
+public class PaypalPaymentService implements PaymentService{
     @Override
     public void processPayment(double amount) {
-        System.out.println("STRIPE");
+        System.out.println("Paypal");
         System.out.println( "Processing payment of " + amount);
     }
 }
