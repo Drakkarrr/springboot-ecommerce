1a61cddb409f0a1b3779a5e6bf087a697f88b8df
Drakkarrr <drakkar.alpha00@gmail.com>
2026-04-26 20:57:21 +0800

    configure bean to code for more control

---
+++ src/main/java/com/store/store/AppConfig.java
@@ -0,0 +1,22 @@
+package com.store.store;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PaymentService stripe() {
+        return new StripePaymentService();
+    }
+
+    @Bean
+    public PaymentService paypal() {
+        return new PaypalPaymentService();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderService(stripe());
+    }
+}
