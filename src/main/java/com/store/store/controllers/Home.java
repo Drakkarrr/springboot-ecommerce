e1a0188575fe9931090a730a945309a2f644c147
Drakkarrr <drakkar.alpha00@gmail.com>
2026-05-25 01:07:44 +0800

    dynamiic data render controller

--- src/main/java/com/store/store/controllers/Home.java
+++ src/main/java/com/store/store/controllers/Home.java
@@ -1,12 +1,14 @@
 package com.store.store.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
     @RequestMapping("/")
-    public String home() {
-        return "Home.html";
+    public String home(Model model) {
+        model.addAttribute("name", "Drakkar");
+        return "Home";
     }
 }
