d5e0b970b0d75c760884dbb9adce58827e67d582
Drakkarrr <drakkar.alpha00@gmail.com>
2026-05-25 01:53:28 +0800

    core user endpoint created fetch individual user

--- src/main/java/com/store/store/controllers/UserController.java
+++ src/main/java/com/store/store/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.store.store.controllers;
 import com.store.store.entities.User;
 import com.store.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class UserController {
     @GetMapping("/users")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
