810f76c84ec28f48a7b163b9ea62c16c3ce8b81a
Drakkarrr <drakkar.alpha00@gmail.com>
2026-05-25 12:38:27 +0800

    revamp response entity to capture null user when not exist

--- src/main/java/com/store/store/controllers/UserController.java
+++ src/main/java/com/store/store/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.store.store.controllers;
 import com.store.store.entities.User;
 import com.store.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +14,21 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @GetMapping("/users/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
