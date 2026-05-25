293f715fd792a44e3bca8f067336a4c86ef5e41c
Drakkarrr <drakkar.alpha00@gmail.com>
2026-05-25 12:51:19 +0800

    implemented a dto to api to restrict the response

--- src/main/java/com/store/store/controllers/UserController.java
+++ src/main/java/com/store/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.store.store.controllers;
 
+import com.store.store.dtos.UserDto;
 import com.store.store.entities.User;
 import com.store.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -19,16 +20,22 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                             .stream()
+                             .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                             .toList();
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/store/store/repositories/UserRepository.java
+++ src/main/java/com/store/store/repositories/UserRepository.java
@@ -2,7 +2,8 @@ package com.store.store.repositories;
 
 
 import com.store.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
